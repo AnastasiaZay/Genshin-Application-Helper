55cf99d1f8112b9745f1a44043b9a289c86338f0
AnastasiaZay <zajch.anastasia@yandex.ru>
2023-05-31 09:53:36 +0400

    Начальный проект

---
+++ app/src/main/java/com/example/genshinapplication/ArtifactProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.genshinapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtifactProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artifact_profile)
+    }
+}
---
+++ app/src/main/java/com/example/genshinapplication/WeaponActivity.kt
@@ -0,0 +1,11 @@
+package com.example.genshinapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeaponActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weapon)
+    }
+}
