b8badf327d4164d556ded645c2ad0c91b6a67891
AnastasiaZay <zajch.anastasia@yandex.ru>
2023-05-31 11:23:50 +0400

    Добавила иконки и структуру
    Позаимствовала иконки у https://github.com/Minoro75/GenshinHelper

---
+++ app/src/main/java/com/example/genshinapplication/activites/ArtifactProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.genshinapplication.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.genshinapplication.R
+
+class ArtifactProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artifact_profile)
+    }
+}
