f9fcf166708cff173e5eed162f461eb8e5433c66
AnastasiaZay <zajch.anastasia@yandex.ru>
2023-06-01 14:30:10 +0400

    Revert "Revert "Апи 2""
    This reverts commit e7b353118faa8659d84c097b3d2c3494aa6583b0.

---
+++ app/src/main/java/com/example/genshinapplication/models/Artifact.kt
@@ -0,0 +1,8 @@
+package com.example.genshinapplication.models
+
+class Artifact() {
+    var name: String? = null
+    var max_rarity: Int? = null
+    var piece_2_bonus: String? = null
+    var piece_4_bonus: String? = null
+}
---
+++ app/src/main/java/com/example/genshinapplication/models/Drop.kt
@@ -0,0 +1,4 @@
+package com.example.genshinapplication.models
+
+class Drop {
+}
