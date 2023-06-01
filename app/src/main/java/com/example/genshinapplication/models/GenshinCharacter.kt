7e5a6d5efddef33942fca200537690491e9342a5
AnastasiaZay <zajch.anastasia@yandex.ru>
2023-06-01 11:11:18 +0400

    Апи 2

--- app/src/main/java/com/example/genshinapplication/models/GenshinCharacter.kt
+++ app/src/main/java/com/example/genshinapplication/models/GenshinCharacter.kt
@@ -1,5 +1,16 @@
 package com.example.genshinapplication.models
 
+import java.time.LocalDate
+
 class GenshinCharacter() {
 
+    // Получаем из джосн
+    var name: String? = null
+    var title: String? = null
+    var vision: String? = null
+    var weapon: String? = null
+    var nation: String? = null
+    var description: String? = null
+    var rarity: Int? = null
+    var birthday: LocalDate? = null
 }
