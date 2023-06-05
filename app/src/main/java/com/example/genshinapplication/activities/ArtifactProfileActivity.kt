8fda9003d642576d69bf2b2103aab84a8cdb0273
AnastasiaZay <zajch.anastasia@yandex.ru>
2023-06-05 15:00:04 +0400

    Страница артефакта [1]
    Добавление на страницу артефакта текста с описанием бонуса комплекта

--- app/src/main/java/com/example/genshinapplication/activities/ArtifactProfileActivity.kt
+++ app/src/main/java/com/example/genshinapplication/activities/ArtifactProfileActivity.kt
@@ -28,6 +28,8 @@ class ArtifactProfileActivity : AppCompatActivity() {
     lateinit var imageViewPlume: ImageView
     lateinit var text4View: TextView
     lateinit var text2View: TextView
+    lateinit var rarView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +41,10 @@ class ArtifactProfileActivity : AppCompatActivity() {
         imageViewPlume = findViewById(R.id.imageViewPlume)
         text4View = findViewById(R.id.PiesArtBonus4)
         text2View = findViewById(R.id.PiesArtBonus2)
-//PiesArtBonus2
-//        cardsMaterials = findViewById(R.id.cardsMaterials)
+        rarView = findViewById(R.id.maxRarArt)
         val nameView = findViewById<TextView>(R.id.artifactName)
         nameView.text = intent.extras!!.getString("name")
         name = intent.extras!!.getString("name")!!.lowercase().replace(" ", "-")
-
 
 
         val client = OkHttpClient()
@@ -80,25 +80,38 @@ class ArtifactProfileActivity : AppCompatActivity() {
             private fun parse(response: String) {
                 val jsonObject = JSONObject(response)
 
-                artifact.name = jsonObject.getString("name")
-//                artifact.piece_2_bonus = jsonObject.getString("2-piece_bonus")
-//                artifact.piece_4_bonus = jsonObject.getString("4-piece_bonus")
-                artifact.max_rarity = jsonObject.getInt("max_rarity")
-                artifact.flowerUri = Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/flower-of-life")
-                artifact.gobletUri = Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/goblet-of-eonothem")
-                artifact.plumeUri = Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/plume-of-death")
-                artifact.sandsUri = Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/sands-of-eon")
 
-                var n = Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/circlet-of-logos")//корону загружаем
+                artifact.name = jsonObject.getString("name")
+                artifact.max_rarity = jsonObject.getInt("max_rarity")
+                artifact.flowerUri =
+                    Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/flower-of-life")
+                artifact.gobletUri =
+                    Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/goblet-of-eonothem")
+                artifact.plumeUri =
+                    Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/plume-of-death")
+                artifact.sandsUri =
+                    Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/sands-of-eon")
+                var n =
+                    Uri.parse("$BASE_URL/artifacts/${name.lowercase()}/circlet-of-logos")//корону загружаем
                 artifact.circletUri = n
-                println(n)
+                artifact.piece_2_bonus = jsonObject.getString("2-piece_bonus")
+                artifact.piece_4_bonus = jsonObject.getString("4-piece_bonus")
+
+
                 runOnUiThread {
-                    //Разберись с добавлением текста
-                    text4View.text = intent.extras!!.getString("4-piece_bonus")
-                    text2View.text = intent.extras!!.getString("2-piece_bonus")
+
+                    //Информация об артефакте
+                    text4View.text =
+                        applicationContext.getString(R.string.artifacts_4_pies_bonus) + " \n" + artifact.piece_4_bonus
+                    text2View.text =
+                        applicationContext.getString(R.string.artifacts_2_pies_bonus) + " \n" + artifact.piece_2_bonus
+                    rarView.text =
+                        applicationContext.getString(R.string.max_rarity) + " \n" + artifact.max_rarity
+
                     Picasso.get().load(n).into(imageViewHat, object :
                         com.squareup.picasso.Callback {
                         override fun onSuccess() {}
+
                         //Если картинки не загрузились
                         override fun onError(e: Exception?) {
                             Picasso.get()
@@ -106,41 +119,22 @@ class ArtifactProfileActivity : AppCompatActivity() {
                                 .into(imageViewHat)
                         }
                     })
-                    imageViewHat.background = ContextCompat.getDrawable(
-                        applicationContext,
-                        when (artifact.max_rarity) {
-                            1 -> R.drawable.background_rarity_1_star
-                            2 -> R.drawable.background_rarity_2_star
-                            3 -> R.drawable.background_rarity_3_star
-                            4 -> R.drawable.background_rarity_4_star
-                            5 -> R.drawable.background_rarity_5_star
-                            else -> R.drawable.background_rarity_5a_star
-                        }
-                    )
-                    Picasso.get().load( artifact.flowerUri).into(imageFlower, object :
+
+                    Picasso.get().load(artifact.flowerUri).into(imageFlower, object :
                         com.squareup.picasso.Callback {
                         override fun onSuccess() {}
+
                         //Если картинки не загрузились
                         override fun onError(e: Exception?) {
                             Picasso.get()
                                 .load("https://static.wikia.nocookie.net/gensin-impact/images/5/59/Traveler_Icon.png/revision/latest")
                                 .into(imageFlower)
                         }
-                    }) //Переделать по-нормальному, чтобы один и тот же код не повторялся по 5 раз
-                    imageFlower.background = ContextCompat.getDrawable(
-                        applicationContext,
-                        when (artifact.max_rarity) {
-                            1 -> R.drawable.background_rarity_1_star
-                            2 -> R.drawable.background_rarity_2_star
-                            3 -> R.drawable.background_rarity_3_star
-                            4 -> R.drawable.background_rarity_4_star
-                            5 -> R.drawable.background_rarity_5_star
-                            else -> R.drawable.background_rarity_5a_star
-                        }
-                    )
+                    })
                     Picasso.get().load(artifact.gobletUri).into(imageViewGobel, object :
                         com.squareup.picasso.Callback {
                         override fun onSuccess() {}
+
                         //Если картинки не загрузились
                         override fun onError(e: Exception?) {
                             Picasso.get()
@@ -148,20 +142,11 @@ class ArtifactProfileActivity : AppCompatActivity() {
                                 .into(imageViewGobel)
                         }
                     })
-                    imageViewGobel.background = ContextCompat.getDrawable(
-                        applicationContext,
-                        when (artifact.max_rarity) {
-                            1 -> R.drawable.background_rarity_1_star
-                            2 -> R.drawable.background_rarity_2_star
-                            3 -> R.drawable.background_rarity_3_star
-                            4 -> R.drawable.background_rarity_4_star
-                            5 -> R.drawable.background_rarity_5_star
-                            else -> R.drawable.background_rarity_5a_star
-                        }
-                    )
+
                     Picasso.get().load(artifact.plumeUri).into(imageViewPlume, object :
                         com.squareup.picasso.Callback {
                         override fun onSuccess() {}
+
                         //Если картинки не загрузились
                         override fun onError(e: Exception?) {
                             Picasso.get()
@@ -169,20 +154,11 @@ class ArtifactProfileActivity : AppCompatActivity() {
                                 .into(imageViewPlume)
                         }
                     })
-                    imageViewPlume.background = ContextCompat.getDrawable(
-                        applicationContext,
-                        when (artifact.max_rarity) {
-                            1 -> R.drawable.background_rarity_1_star
-                            2 -> R.drawable.background_rarity_2_star
-                            3 -> R.drawable.background_rarity_3_star
-                            4 -> R.drawable.background_rarity_4_star
-                            5 -> R.drawable.background_rarity_5_star
-                            else -> R.drawable.background_rarity_5a_star
-                        }
-                    )
+
                     Picasso.get().load(artifact.sandsUri).into(imageSand, object :
                         com.squareup.picasso.Callback {
                         override fun onSuccess() {}
+
                         //Если картинки не загрузились
                         override fun onError(e: Exception?) {
                             Picasso.get()
@@ -190,20 +166,33 @@ class ArtifactProfileActivity : AppCompatActivity() {
                                 .into(imageSand)
                         }
                     })
+
+                    var backgroundPic: Int = when (artifact.max_rarity) {
+                        1 -> R.drawable.background_rarity_1_star
+                        2 -> R.drawable.background_rarity_2_star
+                        3 -> R.drawable.background_rarity_3_star
+                        4 -> R.drawable.background_rarity_4_star
+                        5 -> R.drawable.background_rarity_5_star
+                        else -> R.drawable.background_rarity_5a_star
+                    }
+
                     imageSand.background = ContextCompat.getDrawable(
-                        applicationContext,
-                        when (artifact.max_rarity) {
-                            1 -> R.drawable.background_rarity_1_star
-                            2 -> R.drawable.background_rarity_2_star
-                            3 -> R.drawable.background_rarity_3_star
-                            4 -> R.drawable.background_rarity_4_star
-                            5 -> R.drawable.background_rarity_5_star
-                            else -> R.drawable.background_rarity_5a_star
-                        }
+                        applicationContext, backgroundPic
                     )
+                    imageViewHat.background = ContextCompat.getDrawable(
+                        applicationContext, backgroundPic
+                    )
+                    imageViewPlume.background = ContextCompat.getDrawable(
+                        applicationContext, backgroundPic
+                    )
+                    imageViewGobel.background = ContextCompat.getDrawable(
+                        applicationContext, backgroundPic
+                    )
+                    imageFlower.background = ContextCompat.getDrawable(
+                        applicationContext, backgroundPic
+                    )
+
                 }
-
-
             }
         })
 
